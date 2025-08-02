21e764245bb2aba06734f4c3982ac9b15a5ff41d
Gaston <41041673@fi.unju.edu.ar>
2025-08-01 22:14:07 -0300

    Intento 2 de las imagenes dinamicas

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ function mostrarResultado(index)
     textoResultado.style.visibility = 'visible';
 
     const div = document.querySelector(".resultado_sorteo");
-    div.style.backgroundImage = "url('https://raw.githubusercontent.com/gpelo-data/challenge-amigo-secreto/main/assets/cartel.png')";
+    div.style.backgroundImage = "url('https://gpelo-data.github.io/challenge-amigo-secreto/assets/cartel.png')";
 }
 
 
@@ -158,7 +158,7 @@ function reiniciarSorteo()
     ocultarResultado();
 
     const div = document.querySelector(".resultado_sorteo");
-    div.style.backgroundImage = "url('https://raw.githubusercontent.com/gpelo-data/challenge-amigo-secreto/main/assets/gatito-pixel-sin-fondo.png')";
+    div.style.backgroundImage = "url('https://gpelo-data.github.io/challenge-amigo-secreto/assets/gatito.png')";
 
     return;
 }
